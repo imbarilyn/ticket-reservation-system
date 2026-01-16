d104a5e1546e30a004d267b9b9b642a18e4ce428
dev-lyn <linahmuhonjaimbari@gmail.com>
2026-01-16 18:38:10 +0300

    add http requests

---
+++ src/services/api.service.ts
@@ -0,0 +1,16 @@
+import { API_BASE_URL} from "../config/config.ts";
+
+
+export const apiRequest = async (endpoint: string, options={}) =>{
+
+     const response = await fetch(`${API_BASE_URL}/${endpoint}`, options)
+
+     if(!response.ok){
+         const errorData = await response.json().catch(()=> null)
+         throw  new Error(
+             errorData.message || "Something went wrong please try again"
+         )
+     }
+     console.log('API Response:', response);
+     return  response.json()
+}
