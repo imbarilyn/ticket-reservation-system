cf8546995beb2b2de6d136ced0b3adaaaf316438
dev-lyn <linahmuhonjaimbari@gmail.com>
2026-01-16 18:35:19 +0300

    add TanStack plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import {fileURLToPath} from "node:url";
+import tanstackRouter from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
+
+        tailwindcss(),
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true
+        }),
         react(),
-        tailwindcss()
     ],
     resolve: {
         alias:{
