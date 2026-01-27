480d49d4f2e81cc73d905e54c89a56a68e41a53c
dev-lyn <linahmuhonjaimbari@gmail.com>
2026-01-27 23:35:54 +0300

    update the header

--- src/components/WebsiteHeader.tsx
+++ src/components/WebsiteHeader.tsx
@@ -1,19 +1,19 @@
 import React from "react";
+import {Link} from "@tanstack/react-router";
 
 
 
 
 export const WebsiteHeader: React.FC = () => {
     return (
-        <div className="fixed top-0  right-0 left-0 z-50 flex  justify-between items-center px-8 py-4 bg-dark-one shadow-md">
-            <div>
-                <a href="/public" className="cursor-pointer text-inch-worm-500 text-2xl font-bold">Tiko</a>
-            </div>
-            <div className="text-white flex  items-center gap-8">
+        <div className="sticky top-0 backdrop-blur-md z-50  flex  justify-between items-center px-8 py-4 h-16  shadow-md">
+            <Link to="/">
+                <span className="cursor-pointer text-pumpkin-500 text-2xl font-bold">Tik</span><span className="text-picton-blue-500 text-2xl font-boldcursor-pointer ">o</span>
+
+            </Link>
+            <div className=" flex  items-center gap-8">
                     <p className="cursor-pointer">Contact us</p>
-                    <button className=" cursor-pointer btn-ghost btn-sm bg-inch-worm-500 px-4 py-2 rounded-xl">
-                        <span className="text-black">Sell Event</span>
-                    </button>
+
             </div>
         </div>
     )
