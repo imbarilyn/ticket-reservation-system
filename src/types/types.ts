e5351b6689655ff79c07cafa0d4a84a2ad78c96d
dev-lyn <linahmuhonjaimbari@gmail.com>
2026-01-29 02:56:56 +0300

    add RouterContext type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,5 @@
+import type {QueryClient} from "@tanstack/react-query";
+
 export interface Event {
     event_id: number;
     name: string;
@@ -5,6 +7,21 @@ export interface Event {
     location: string;
     amount: number;
     description: string;
-    startTime: string;
-    endTime: string;
+    start_time: string;
+    end_time: string;
+}
+
+export interface ColorContentType {
+    bgColor: string;
+    darkBgColor: string;
+    bgHoverColor: string;
+    bgIconColor: string;
+    textColor: string;
+    setBgColor: (color: string) => void;
+
+}
+
+export interface RouterContext {
+    queryClient: QueryClient;
+
 }
