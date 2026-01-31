e06be797436b8cb878c86391cf713fd689109fe5
dev-lyn <linahmuhonjaimbari@gmail.com>
2026-01-31 07:56:33 +0300

    add usequery hook logic

--- src/routes/(events)/index.tsx
+++ src/routes/(events)/index.tsx
@@ -1,66 +1,55 @@
-import { fetchEvents} from "../../services/events.service";
-import {createFileRoute, Link} from "@tanstack/react-router";
-import {type  Event } from "../../types/types.ts";
-import {ErrorComponent} from "../../components/ErrorComponent.tsx";
-import  screenshoot2 from "../../assets/images/screenshoot4.png"
+import {createFileRoute} from "@tanstack/react-router";
+import {type  Event} from "../../types/types.ts";
 import {HeroSection} from "../../components/HeroSection.tsx";
+import {fetchEvents} from "../../services/events.service.ts";
+import {EventListComponent} from './-components/eventList.tsx'
+import {queryOptions, useQuery} from "@tanstack/react-query";
+import {LoadingShimmer} from "../../components/LoadingShimmer.tsx";
 
-
-
-
+const eventsQueryOptions = queryOptions({
+    queryKey: ["events"],
+    queryFn: fetchEvents,
+})
 export const Route = createFileRoute('/(events)/')({
-    loader: fetchEvents,
+    loader: async ({context}) => {
+        const events = await context.queryClient.ensureQueryData(eventsQueryOptions)
+        return {events};
+    },
     component: EventsLayoutComponent,
-    errorComponent: ErrorComponent
+
 })
 
-
 function EventsLayoutComponent() {
-    const events: Event []   = Route.useLoaderData();
-    console.log("Here are the events:", events.length);
-
-    const buyTicket = () => {
-
+    const {data: events = [], isPending, isError, error, refetch} = useQuery(eventsQueryOptions)
+    if (isPending) {
+        return <LoadingShimmer/>
     }
+    if (isError) {
+        return (
+            <div className="">
+                <p>Error: {error instanceof Error ? error.message : 'something went wrong'}</p>
+                <button onClick={() => refetch()} className="bg-pumpkin-300 btn-sm px-4 py-2">Retry</button>
+            </div>
+        )
+    }
+
+
     return (
         <div className="space-y-10">
-            <HeroSection />
-            <div className="cursor-pointer gap-4 w-10/12  mx-auto flex">
+            <HeroSection/>
+            <div className="w-9/12  mx-auto space-y-4  ">
+                <p className="text-picton-blue-950 text-2xl font-semibold">Upcoming Events</p>
                 {
-                    events.length>0?  events.map((envt: Event)=> (
-                        <div
-                            key={envt.event_id}
-                            className=" group block max-w-sm p-6 border border-inch-worm-200 rounded-[22px]">
-                            <div className="border relative  border-inch-worm-100 rounded-[16px]">
-
-                               <div className="group-hover:opacity-10 opacity-90  transition-opacity delay-200 duration-300 ease-in-out rounded-[16px] overflow-hidden">
-                                   <img className="object-cover" src={screenshoot2} alt=""/>
-                               </div>
-                                    params={{eventId: String(envt.event_id)}}
-                                    to="/events/$eventId" className="hidden group-hover:block ">
-                                    <button onClick={()=>buyTicket} className="w-full py-2 bg-inch-worm-200 text-black rounded-lg">Buy Tickets</button>
-
-                            </div>
-                            <div>
-                                <h5 className="mt-6 mb-2 text-2xl font-semibold tracking-tight text-heading text-white">{envt.name}</h5>
-                                <div className="text-white">
-                                    <div className="flex gap-4">
-                                        <span className="opacity-40">Date</span>
-                                        <span>{envt.date}</span>
-                                    </div>
-                                    <div className="flex gap-4">
-                                        <span className="opacity-40">Venue</span>
-                                        <span>{envt.location}</span>
-                                    </div>
-                                    <div className="flex gap-4">
-                                        <span className="opacity-40">Price</span>
-                                        <span>{envt.amount}</span>
-                                    </div>
-                                </div>
-
-                            </div>
+                    !isPending ?
+                        <div className="relative cursor-pointer gap-8 flex flex-wrap">
+                            {
+                                events.map((event: Event) => (
+                                    <EventListComponent key={event.event_id} event={event}/>
+                                ))
+                            }
                         </div>
-                    )) : <p className="text-white">No events available yet.</p>
+                        :
+                        <LoadingShimmer/>
                 }
             </div>
         </div>
