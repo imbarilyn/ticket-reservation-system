28f7acd915cf36e72098bbf63f79908de1477bb2
dev-lyn <linahmuhonjaimbari@gmail.com>
2026-02-03 20:38:49 +0300

    update ui

--- src/routes/(events)/events.$eventId.tsx
+++ src/routes/(events)/events.$eventId.tsx
@@ -57,47 +57,47 @@ function EventDetailComponent() {
         })
 
     }
-    const iconColor = '#defb9b'
+    const iconColor = '#f97316'
     return (
-        <div className="xl:pt-48 pt-28 2xl:w-8/12 w-10/12  gap-4 mx-auto xl:flex xl:justify-between space-y-16">
-            <div className="xl:max-w-lg space-y-10">
-                <h1 className="text-white text-5xl font-bold">{event.name}</h1>
-                <p className="text-white text-lg">{event.description}</p>
+        <div className="xl:pt-28 pt-12 2xl:w-8/12 w-10/12   gap-4 mx-auto lg:flex justify-between space-y-16">
+            <div className="xl:w-lg space-y-10">
+                <h1 className=" text-5xl font-bold">{event.name}</h1>
+                <p className=" text-lg">{event.description}</p>
                 <div className="flex gap-4">
                     <button
                         onClick={(e)=>{
                             e.stopPropagation()
                             checkoutPayment(event.event_id)
                         }}
-                        className="btn btn-sm text-white bg-linear-to-r from-inch-worm-900 to-inch-worm-600 px-8 py-4 rounded-lg font-bold">Buy
+                        className="cursor-pointer btn btn-sm  bg-linear-to-r  from-serenade-600 to-pumpkin-300  px-8 py-4 rounded-lg font-bold">Buy
                         ticket
                     </button>
                     <button
-                        className="btn btn-sm flex  gap-2 text-white border-[1px] border-stone-600 px-6 py-4 rounded-lg font-bold">
+                        className="btn btn-sm cursor-pointer flex  gap-2  border-[1px] border-stone-600 px-6 py-4 rounded-lg font-bold">
                         <Share2/>
                         <span>Share event</span>
                     </button>
                 </div>
                 <div className="space-y-4 w-full">
-                    <div className="flex  gap-6 border-[1px] border-bg-slate-50 p-4 rounded-lg shadow-lg">
+                    <div className="flex border-[2px] border-gray-100  gap-6 p-4 rounded-2xl shadow-lg">
                         <Calendar color={iconColor}/>
                         <div>
-                            <p className="text-white text-sm"> {formatterFn(event.date, 'date')}</p>
-                            <p className="text-white text-md">Gates open at {formatterFn(event.start_time, 'time')}</p>
+                            <p className=" text-sm"> {formatterFn(event.date, 'date')}</p>
+                            <p className=" text-md">Gates open at {formatterFn(event.start_time, 'time')}</p>
                         </div>
                     </div>
-                    <div className="flex  gap-6 border-[1px] border-bg-slate-50 p-4 rounded-lg shadow-lg">
+                    <div className="flex  gap-6 border-[2px] border-gray-100 p-4 rounded-2xl shadow-lg">
                         <MapPin color={iconColor}/>
                         <div>
-                            <p className="text-white text-lg">{event.location}</p>
-                            <p className="text-white text-md">See direction</p>
+                            <p className=" text-lg">{event.location}</p>
+                            <p className=" text-md">See direction</p>
                         </div>
                     </div>
                 </div>
                 {/* Render other event details here */}
             </div>
-            <div className="flex justify-center xl:justify-end">
-                <img src={screenshoot2} className="object-cover rounded-xl h-[500px]"/>
+            <div className="flex   justify-center   xl:justify-end">
+                <img src={screenshoot2} className="object-cover rounded-xl h-[500px]  max-w-sm xl:max-w-lg "/>
 
             </div>
         </div>
